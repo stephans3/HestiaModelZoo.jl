f53508f29d420ee0c45f1898928b05850c4c77fb
Documenter.jl <documenter@juliadocs.github.io>
2023-11-22 16:45:32 +0000

    build based on d4fb5e6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HestiaModelZoo","category":"page"},{"location":"#HestiaModelZoo","page":"Home","title":"HestiaModelZoo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HestiaModelZoo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HestiaModelZoo]","category":"page"}]
+}
